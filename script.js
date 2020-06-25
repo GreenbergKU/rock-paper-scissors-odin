9caab8ed560a30f77bca1f8856ab386421ad8043
SuzieQ <suzieqwolf@gmail.com>
2020-06-24 23:19:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,101 +1,80 @@
-var random = ['ROCK', 'PAPER', 'SCISSORS'];
+var playOptions  = ['ROCK', 'PAPER', 'SCISSORS'];
 var userPlay;
 
 document.onload = loadGame();
 
-function promptUser() {
-  console.log("@prompt")
-  var userPrompt = prompt("ROCK, PAPER or SCISSORS, type your choice in the box below!");
-  userPlay = userPrompt !== null ? userPrompt : "";
-  userPlay = userPlay.toUpperCase();
-  console.log("userPlay: ", userPlay, "userPrompt: ", userPrompt);
-}
-  // if (userPrompt === null) {
-  //   userPrompt = "";
-  // };
-  // userPlay = userPrompt;
-
-function computerPlay(arr) { 
-  console.log("@ computerPlay")
-  var rpsRandoms = [];       
-  var randomIndex;
-   for (var i = 0; i < arr.length - 1; i++) {
-    randomIndex = Math.floor(Math.random() * arr.length);
-    rpsRandom = arr[randomIndex];
-    rpsRandoms.push(rpsRandom);
-    //console.log("rpsRandom:", rpsRandom);
-  } 
-  //console.log("rpsRandoms:", rpsRandoms);
-  return rpsRandoms
-}
-
-function playRPS(arr) {
-  console.log("@ playRPS")
-  var user, message;
-  var computer = arr[0];
-  user = userPlay == "ROCK" || userPlay == "PAPER" || userPlay == "SCISSORS" ? userPlay : arr[1];
-  // user: manual input or randomly generated
-  console.log("u", user, "c", computer);
-  message = computer === user ? `TIE! Both of you were ${user}!`
-  : user === 'ROCK' && computer === 'SCISSORS' || user === 'PAPER' && computer === 'ROCK' ||
-    user === 'SCISSORS' && computer === 'PAPER' ? 
-   `User wins! ${user} beats ${computer}!`
-    : `Computer wins! ${computer} beats ${user}!`;
-  //console.log(message.includes('User'));
-  console.log("message = ", message);
-  return message;
-}
-
 function loadGame() {
-  console.log("@ game");
-  var rounds = [1, 2, 3, 4, 5];
+  var gameRounds = [1, 2, 3, 4, 5];
   var userWins = 0;
   var computerWins = 0;
   var gameMessage = "";
-  var computerRandom;
-  for (var i = 0; i < rounds.length; i++) {
+  for (var i = 0; i < gameRounds.length; i++) {
     promptUser();
-    gameMessage = playRPS(computerPlay(random));
-      //  computerRandom = computerPlay(random);
-      //  gameMessage = playRPS(computerRandom, userInput);
-    console.log("gameMessage: ", !gameMessage.includes("TIE"), gameMessage.includes("User"));
+    gameMessage = oneRoundRPS(computerPlay(playOptions));
     if (!gameMessage.includes("TIE")) {      
-      gameMessage.includes("User") ? userWins++ : computerWins ++;
+      gameMessage.includes("User") ? userWins++ : computerWins++;
     };
     alert(`${gameMessage} score: USER ${userWins}pts vs COMPUTER ${computerWins}pts`);
   };
-  console.log(userWins, computerWins);
   checkForDraw(userWins, computerWins);
 }
 
-function checkForDraw(num1, num2) {
-    console.log("@ checkForDraw");
-  num1 === num2 ? tieBreakRound(num1, num2) : displayGameWinner(num1, num2);
+function promptUser() {
+  var userPrompt = prompt("ROCK, PAPER or SCISSORS? Enter your choice below!");
+  userPlay = (userPrompt !== null ? userPrompt : "").toUpperCase();
 }
 
-function tieBreakRound(num1, num2) {
-    console.log("@ tieBreakRound");
-    var gameMessage = playRPS(computerPlay(random));
-  if (!gameMessage.includes("TIE")) {         
-    gameMessage.includes("User") ? num1++ : num2++;
+function computerPlay(playOptions) { 
+  var rpsRandoms = [];       
+  var randomIndex;
+   for (var i = 0; i < playOptions.length - 1; i++) {
+    randomIndex = Math.floor(Math.random() * playOptions.length);
+    singleRandomRPS = playOptions[randomIndex];
+    rpsRandoms.push(singleRandomRPS);
   };
-  checkForDraw(num1, num2);
+  return rpsRandoms;
 }
 
-function displayGameWinner(num1, num2) {
-    console.log("@ displayGameWinner");
+function oneRoundRPS(randomOptions) {
+  var user, roundMessage;
+  var computer = randomOptions[0];
+  user = userPlay === "ROCK" || userPlay === "PAPER" || userPlay === "SCISSORS" 
+  ? userPlay : randomOptions[1];
+  roundMessage = computer === user ? `TIE! Both of you were ${user}!`
+  : user === 'ROCK' && computer === 'SCISSORS' || 
+    user === 'PAPER' && computer === 'ROCK' ||
+    user === 'SCISSORS' && computer === 'PAPER' 
+  ? `User wins! ${user} beats ${computer}!` 
+    : `Computer wins! ${computer} beats ${user}!`;
+  return roundMessage;
+}
+
+function checkForDraw(userScore, compScore) {
+  userScore === compScore ? tieBreakRound(userScore, compScore) : displayGameWinner(userScore, compScore);
+}
+
+function tieBreakRound(userScore, compScore) {
+    var gameMessage = oneRoundRPS(computerPlay(playOptions));
+  if (!gameMessage.includes("TIE")) {         
+    gameMessage.includes("User") ? userScore++ : compScore++;
+  };
+  checkForDraw(userScore, compScore);
+}
+
+function displayGameWinner(userScore, compScore) {
   var gameWinner, winnerScore, loserScore, winMessage;
-  gameWinner = num1 > num2 ? 'user' : 'computer';
-  winnerScore = gameWinner === 'user' ? num1 : num2;
-  loserScore = gameWinner === 'user' ? num2 : num1;
-  winMessage = `${gameWinner.toUpperCase()} WINS! SCORE: ${winnerScore} to ${loserScore}!!!`
-  //console.log("winMessage = ", winMessage);
-  alert(`${gameWinner.toUpperCase()} WINS! score: ${winnerScore} to ${loserScore}!!!`);
+  gameWinner = userScore > compScore ? 'user' : 'computer';
+  winnerScore = gameWinner === 'user' ? userScore : compScore;
+  loserScore = gameWinner === 'user' ? compScore : userScore;
+  winMessage = `${gameWinner.toUpperCase()} WINS! SCORE: ${winnerScore} to ${loserScore}!`;
+  alert(winMessage);
+  promptPlayAgain();
 }
 
-  // if (userPlay === null) {alert("null")};
-  // if (userPlay === "") {alert("emptyString")};
-  // if (userPlay) {alert("true")};
-  // if (!userPlay) {alert("!userPlay"); console.log("!:", !userPlay)};
-  // if (userPlay == "ROCK" || userPlay == "PAPER" || userPlay == "SCISSORS") {alert("game() ON!");}
-  // var user = userPlay || arr[1].toUpperCase();
+function promptPlayAgain() {
+  var playAgainPrompt = prompt("PLAY AGAIN?? click OK to CONTINUE, click CANCEL to EXIT GAME", "type YES to continue or NO to exit game");
+  if (playAgainPrompt !== null) {
+  userPlay = playAgainPrompt.toUpperCase() !== "NO" ?
+    userPlay === "YES" || "" ? loadGame() : null : null;
+  };
+}
